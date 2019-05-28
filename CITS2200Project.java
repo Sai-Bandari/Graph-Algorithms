ae5d12aed653c4088cdaa838e2ab59d0b591765a
Sai-Bandari <49908875+Sai-Bandari@users.noreply.github.com>
2019-05-28 15:16:59 +0800

    Add files via upload

--- CITS2200Project.java
+++ CITS2200Project.java
@@ -1,3 +1,11 @@
+import java.util
+import java.util.ArrayList;
+import java.util.LinkedList;*
+
+
+
+
+
 public interface CITS2200Project {
 	/**
 	 * Adds an edge to the Wikipedia page graph. If the pages do not
@@ -6,17 +14,108 @@ public interface CITS2200Project {
 	 * @param urlFrom the URL which has a link to urlTo.
 	 * @param urlTo the URL which urlFrom has a link to.
 	 */
-	public void addEdge(String urlFrom, String urlTo);
+	//We must first create a graph 
 
-	/**
-	 * Finds the shorest path in number of links between two pages.
-	 * If there is no path, returns -1.
-	 * 
-	 * @param urlFrom the URL where the path should start.
-	 * @param urlTo the URL where the path should end.
-	 * @return the legnth of the shorest path in number of links followed.
-	 */
-	public int getShortestPath(String urlFrom, String urlTo);
+	class Graph{
+		private int[][] edgeMatrix;
+		private int numberOfVertices;
+
+
+		private Graph(int paramInt) {
+			if (paramInt < 1) {
+				this.numberOfVertices = 0; 
+			}
+
+			else { 
+				this.numberOfVertices = paramInt;
+			}
+
+			this.edgeMatrix = new int[this.numberOfVertices][this.numberOfVertices];
+		}
+
+
+
+
+
+		public void addEdge(String urlFrom, String urlTo) {
+			if (0 <= paramInt1 && 0 <= paramInt2 && paramInt1 <= this.numberOfVertices && paramInt2 <= this.numberOfVertices) {
+				if (paramInt3 < 1) { paramInt3 = 0; }
+				else if (!this.weighted) { paramInt3 = 1; }
+				this.edgeMatrix[paramInt1][paramInt2] = paramInt3;
+				if (!this.directed) this.edgeMatrix[paramInt2][paramInt1] = paramInt3; 
+			} else {
+				throw new RuntimeException("Vertex out of bounds");
+			} 
+
+
+
+		}
+
+
+		/**
+		 * Finds the shorest path in number of links between two pages.
+		 * If there is no path, returns -1.
+		 * 
+		 * @param urlFrom the URL where the path should start.
+		 * @param urlTo the URL where the path should end.
+		 * @return the legnth of the shorest path in number of links followed.
+		 */
+
+		public int getShortestPath(String urlFrom, String urlTo) {
+			//Number of vertices in the graph is stored
+			int nVertex = g.getNumberOfVertices();
+
+			int[][] Weight = g.getEdgeMatrix();
+
+			int[] dist = new int[nVertex];
+
+			int[] travelled = new int[nVertex];
+
+
+			Arrays.fill(travelled, 0);
+
+			Arrays.fill(dist, -1);
+
+
+
+			PriorityQueue<Edge> q = new PriorityQueue<Edge>();
+
+			q.add((PathImp<E>.Edge)new Edge(source, 0));
+
+			dist[source] = 0;
+
+			while (!q.isEmpty()) {	
+
+				int vertex = q.remove().vertex;
+
+				if (travelled[vertex] != 0) {
+
+					continue;
+
+				}
+
+				travelled[vertex] = 1;
+
+				for (int i = 0; i < nVertex; i++) {
+
+					int weight = Weight[vertex][i];
+
+					if (weight != 0 && travelled[i] != 1) {
+
+						if (dist[i] > dist[vertex] + weight || dist[i] == -1) {
+
+							dist[i] = dist[vertex] + weight;
+
+							q.add((PathImp<E>.Edge)new Edge(i,dist[i]));
+						}
+					}
+				}
+			}
+
+
+			return dist;
+		}
+	}
 
 	/**
 	 * Finds all the centers of the page graph. The order of pages
